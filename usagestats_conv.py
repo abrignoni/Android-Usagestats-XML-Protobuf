62543f9dec36f7a9cfbdce189cc1679707bf2ced
Brigs <abrignoni@gmail.com>
2020-02-15 09:59:46 -0500

    Add files via upload

--- usagestats_conv.py
+++ usagestats_conv.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET  
 import glob, os, sqlite3, os, sys, re, json
 import protobuf.usagestatsservice_pb2
+import usagestatsservice_pb2
 from enum import IntEnum
 
 class EventType(IntEnum):
@@ -179,6 +180,8 @@ for filename in glob.iglob(script_dir+r'/usagestats/**', recursive=True):
 					err = 1
 					#print(filename)
 				if stats:
+					print('Processing: '+filename)
+					print('')
 					AddEntriesToDb(stats, db)
 					continue
 			
